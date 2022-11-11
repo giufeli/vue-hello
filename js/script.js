1735594cec0aa95a93c01fee1c48e1ea4ce04134
giufeli <giuliofelicetti99@gmail.com>
2022-11-11 14:50:01 +0100

    Commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = new Vue ({ 
+    el: '#root',
+    data: {
+        message: 'Hello Vue!',
+        image: 'img/wp7134662.webp',
+        classTitle: 'title',
+        classContainer: 'container'
+    }
+});
